a479a9849e35ee39acca20dd67fea0d3864a1fa2
Kaviarasan K K <kaviarasankk@gmail.com>
2018-07-23 17:21:25 +0530

    Phoenix

--- ng/src/app/auth/auth.route.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-// Component
-import { LoginFormComponent, LogoutComponent } from './auth.component';
-
-export const AuthRoutes: Routes = [
-    { path:'login', component:LoginFormComponent },
-    { path:'logout', component:LogoutComponent}
-];
--- ng/src/app/category/category.ts
+++
@@ -1,9 +0,0 @@
-export class Category{
-	id:number;
-	category_id: number;
-	family_id: number;
-	user_id: number;
-	name: string;
-	hexcolor:string;
-	description:string;
-}
